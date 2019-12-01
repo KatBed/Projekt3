4ce3c41e21a267907a1ef4c34778c512fa64b6a8
KB <bjakbednarek@gmail.com>
2019-12-01 19:58:41 +0100

    Dodanie zmian do Projektu3

--- Projekt3/Projekt3/Program.cs
+++ Projekt3/Projekt3/Program.cs
@@ -4,12 +4,14 @@ namespace Zadanie3
 {
     class Program
     {
+        private static int integer;
+
         static void Main(string[] args)
         {
             Console.Write("Wprowadź dowolną liczbę całkowitą: ");
             string input = Console.ReadLine();
             Console.WriteLine();
-            if (int.TryParse(input, out integer)
+            if (int.TryParse(input, out integer))
             {
                 Console.WriteLine("Wprowadzono liczbę całkowitą '" + input + "'");
             }
@@ -20,10 +22,11 @@ namespace Zadanie3
                     input = "(pusty string)";
                 }
                 Console.WriteLine("Wprowadzona wartośc nie jest liczbą całkowitą!" + Environment.NewLine + "Wprowadzono '" + input + "'");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Brawo! Trzecie zadanie zrobione :)");
+            Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+            Console.ReadKey();
         }
-        Console.WriteLine();
-        Console.WriteLine("Brawo! Trzecie zadanie zrobione :)");
-        Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
-        Console.ReadKey();
     }
 }
